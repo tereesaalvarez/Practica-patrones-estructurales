7a5e71d76119733b822ee84c37626b35d3428196
tereesaalvarez <alvdeporteresa@gmail.com>
2023-11-21 14:14:23 +0100

    Ejercicio2

--- Ejercicio1/PizzaComposite/Component.py
+++ Ejercicio1/PizzaComposite/Component.py
@@ -6,12 +6,11 @@ from typing import List
 class MenuComponent(ABC):
 
     @property
-    def parent(self) -> MenuComponent:
-        return self._parent
+    def menu(self) -> MenuComponent:
+        return self._menu
 
-    @parent.setter
-    def parent(self, parent: MenuComponent):
-
+    @menu.setter
+    def menu(self, parent: MenuComponent):
         self._parent = parent
 
     def add(self, component: MenuComponent) -> None:
@@ -25,37 +24,29 @@ class MenuComponent(ABC):
         return False
 
     @abstractmethod
-    def operation(self) -> str:
+    def listar_componenetes(self) -> str:
         pass
 
+    @abstractmethod
+    def obtener_precio(self) -> str:
+        pass
 
-class Leaf(Component):
+class Leaf(MenuComponent):
 
     def operation(self) -> str:
         return "Leaf"
 
 
-class Composite(Component):
+class Postre(MenuComponent):
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
 
-    def __init__(self) -> None:
-        self._children: List[Component] = []
+    def listar_componentes(self):
+        print(f"Postre: {self.nombre}")
 
-    def add(self, component: Component) -> None:
-        self._children.append(component)
-        component.parent = self
-
-    def remove(self, component: Component) -> None:
-        self._children.remove(component)
-        component.parent = None
-
-    def is_composite(self) -> bool:
-        return True
-
-    def operation(self) -> str:
-        results = []
-        for child in self._children:
-            results.append(child.operation())
-        return f"Branch({'+'.join(results)})"
+    def obtener_precio(self) -> float:
+        return self.precio
 
 
 def client_code(component: Component) -> None:
@@ -72,29 +63,3 @@ def client_code2(component1: Component, component2: Component) -> None:
     print(f"RESULT: {component1.operation()}", end="")
 
 
-if __name__ == "__main__":
-    # This way the client code can support the simple leaf components...
-    simple = Leaf()
-    print("Client: I've got a simple component:")
-    client_code(simple)
-    print("\n")
-
-    # ...as well as the complex composites.
-    tree = Composite()
-
-    branch1 = Composite()
-    branch1.add(Leaf())
-    branch1.add(Leaf())
-
-    branch2 = Composite()
-    branch2.add(Leaf())
-
-    tree.add(branch1)
-    tree.add(branch2)
-
-    print("Client: Now I've got a composite tree:")
-    client_code(tree)
-    print("\n")
-
-    print("Client: I don't need to check the components classes even when managing the tree:")
-    client_code2(tree, simple)
