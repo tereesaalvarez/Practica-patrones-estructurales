a81b3cc893293fb4bccecd030433def53b21e5e9
tereesaalvarez <alvdeporteresa@gmail.com>
2023-11-27 11:53:16 +0100

    usuario

---
+++ Ejercicio2/BaseDatos/usuario.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self, usuario, contraseña):
+        self.usuario = usuario
+        self.contraseña = contraseña
+
+class UsuarioDatabase:
+    def __init__(self):
+        self.usuarios = []
+
+    def añadir_usuario(self, usuario):
+        self.usuarios.append(usuario)
+
+    def encontrar_usuario(self, nombre_usuario, contraseña):
+        for usuario in self.usuarios:
+            if usuario.nombre_usuario == nombre_usuario and usuario.contraseña == contraseña:
+                return True
+        return False
