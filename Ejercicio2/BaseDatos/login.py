756988020ce6e740c8e2e77e37880d673906ad15
tereesaalvarez <alvdeporteresa@gmail.com>
2023-11-27 11:58:41 +0100

    login

---
+++ Ejercicio2/BaseDatos/login.py
@@ -0,0 +1,28 @@
+import sqlite3
+from datetime import datetime
+
+class AccederDatabase:
+    def __init__(self):
+        self.connection = sqlite3.connect('basedatos.db')
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS usuarios_log (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre_usuario VARCHAR(50) UNIQUE,
+                contraseña VARCHAR(50),
+                fecha_creacion DATETIME
+            )
+        ''')
+        self.connection.commit()
+
+    def logear(self, usuario, contraseña):
+        fecha = datetime.now()
+        self.cursor.execute('''
+            INSERT INTO usuarios_log VALUES (NULL, ?, ?, ?)
+        ''', (usuario, contraseña, fecha))
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
+
+    
